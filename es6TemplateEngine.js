18ad199d5d1bbf373d9f19cd100589ff3e698613
Marcuslyj <marcuslyj999@gmail.com>
2020-02-01 14:40:40 +0800

    es6模板字符串实现模板引擎，性能好

---
+++ es6TemplateEngine.js
@@ -0,0 +1,29 @@
+// es6模板字符串实现模板引擎
+// 要素
+// xss过滤
+
+
+const user = {
+    name: '<sctipt/>'
+}
+const result = `<h2>${user.name}</h2>`
+
+const vm = require('vm')
+// 运行在沙箱
+console.log(vm.runInNewContext('`<h2>${user.name}</h2>`', {
+    user,
+    // xss过滤函数
+    _: function (markup) {
+        if (!markup) return ''
+        return String(markup)
+            .replace(/&/g, '&amp;')
+            .replace(/ /g, '&nbsp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/\r{0,}\n/g, '<br/>')
+    }
+}))
+// const template = '<h2><%= user.name %></h2>'
+// ejs.render(template, user)
